4a0aa4c35f0d6c864e44f01558d4731371ba96e2
andyberaldcatelo <andy.berald@yahoo.fr>
2020-07-07 07:29:14 +0200

    2020 07 07 Rendering Adjacent JSX Elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import classes from './Person.module.css';
+import Auxiliary from '../../../hoc/Auxiliary';
+//import classes from './Person.module.css';
 
 // Pour utiliser les lifecycle hooks, il faut absolument avoir un component class-based
 
@@ -7,17 +8,18 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
+      <Auxiliary>
+        <p key='i1' onClick={this.props.click}>
           I'm {this.props.name} and I'm {this.props.age}
         </p>
         <input
+          key='i2'
           type='text'
           defaultValue={this.props.name}
           onChange={this.props.changed}
         />
-        <p>{this.props.children}</p>
-      </div>
+        <p key='i3'>{this.props.children}</p>
+      </Auxiliary>
     );
   }
 }
