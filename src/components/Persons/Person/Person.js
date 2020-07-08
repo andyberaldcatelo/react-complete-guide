88b333a4bcc0b897f7f59e6ef083ff7f284a913a
andyberaldcatelo <andy.berald@yahoo.fr>
2020-07-08 09:13:46 +0200

    2020 07 08 Using References

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,15 @@ import PropTypes from 'prop-types';
 // Pour utiliser les lifecycle hooks, il faut absolument avoir un component class-based
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
@@ -15,6 +24,7 @@ class Person extends Component {
           I'm {this.props.name} and I'm {this.props.age} years old
         </p>
         <input
+          ref={this.inputElementRef}
           key='i2'
           type='text'
           defaultValue={this.props.name}
